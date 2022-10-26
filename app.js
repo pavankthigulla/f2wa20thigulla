50f27ec168aafd4549caf84ff0b4f24cbb4995d5
pavankthigulla <111797182+pavankthigulla@users.noreply.github.com>
2022-10-26 03:04:24 -0500

    modified users.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mydataRouter=require('./routes/mydata')
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,9 +38,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-app.listen(3000, function () {
-  console.log("Server is running on localhost:3000");
 });
 
 module.exports = app;
